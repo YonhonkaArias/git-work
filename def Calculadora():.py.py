f5498d246f9500170606d17c2bc544de478ad782
YonhonkaArias <Yonhonkaarias@gmail.com>
2026-01-28 11:44:16 -0400

    multi div added

--- def Calculadora():.py.py
+++ def Calculadora():.py.py
@@ -1,8 +1,10 @@
 def Calculadora():
     print("1. suma")
     print("2. resta")
+    print("multiplicacion")
+    print("division")
 
-    option = input("Elige una opción (1-2)")
+    option = input("Elige una opción (1-4)")
 
     num1 = float(input("Pon el primer numero:"))
     num2 = float(input("Pon el primsegundoer numero:"))
@@ -13,4 +15,14 @@ def Calculadora():
     elif option == "2":
         result = num1 - num2
         print("Resultado:", result)
+    elif option == "3":
+        result = num1 * num2
+        print("Resultado:", result)
+    elif option == "4":
+        if num2 == 0:
+            print("no se puede dividir por 0")
+    else:
+        result = num1/num2
+        print("Resultado:", result)
 Calculadora()
+
